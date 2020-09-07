c17f13fe2a915864f5f172b1ae2494cea3f55fae
WenqiangLiao <18840965351@163.com>
2020-09-07 16:01:49 +0800

    [Wenqiang Liao] feat: get data of student list

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,36 @@ import React, { Component } from 'react';
 import './App.scss';
 import GroupSeperate from '../components/groupSeperate/GroupSeperate';
 import GroupList from '../components/groupList/GroupList';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) { 
+    super(props);
+
+    this.state = {
+      groupList: []
+    }
+  }
+
+  componentDidMount() { 
+    this.getStudentList();
+  }
+
+  getStudentList = () => { 
+    axios({
+      url: 'http://localhost:8080/list',
+      method: 'get'
+    })
+      .then(Response => {
+        this.setState({
+          groupList: Response.data
+        })
+      })
+      .catch((e) => { 
+        console.log(e);
+      })
+  }
+
   render() {
     return (
       <div className="App">
