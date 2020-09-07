add804148b713150d014e210928d25aa5a71969a
WenqiangLiao <18840965351@163.com>
2020-09-07 15:02:31 +0800

    [Wenqiang Liao] feat: add header to group seperate

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import GroupSeperate from '../components/GroupSeperate';
 
 class App extends Component {
   render() {
     return (
-      <div data-testid="app" className="App">
-        Hello World
+      <div className="App">
+        <GroupSeperate />
       </div>
     );
   }
---
+++ src/components/GroupSeperate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './groupSeperate.css';
+
+class groupSeperate extends Component {
+  render() {
+    return (
+      <div className="groupSeperate">
+        <header>
+          <h3>分组列表</h3>
+          <button>分组学员</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default groupSeperate;
